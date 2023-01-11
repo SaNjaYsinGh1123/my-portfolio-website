017d6a36e7c992efea5d3b0addada3aa8b780ed2
SaNjaYsinGh1123 <sanjayfbd93544@gmail.com>
2023-01-11 16:48:39 +0530

    try 20 put all music.js code in music.ejs  (with app.use('/sounds'), sounds music.js, socket_engine,chat_socket path in mongostore and view ,4 update with home-portfoilio.ejs) vercel.json and delete echo build

--- assets/javascript/music.js
+++ assets/javascript/music.js
@@ -1,150 +1,150 @@
 console.log('welcome');
 
-let songIndex = 0;
-let audioElement = new Audio('/sounds/song/1.mp3');
-let currentPlay = document.getElementById('current-song'); 
-let prevPlay = document.getElementById('prev-song'); 
-let nextPlay = document.getElementById('next-song'); 
-let progressBar = document.getElementById('slider');
-let container = document.getElementsByClassName('container')[0];
-let songBannergif = document.getElementsByClassName('songBannergif')[0];
-let songGif = document.getElementById('song-gif');
+// let songIndex = 0;
+// let audioElement = new Audio('/sounds/song/1.mp3');
+// let currentPlay = document.getElementById('current-song'); 
+// let prevPlay = document.getElementById('prev-song'); 
+// let nextPlay = document.getElementById('next-song'); 
+// let progressBar = document.getElementById('slider');
+// let container = document.getElementsByClassName('container')[0];
+// let songBannergif = document.getElementsByClassName('songBannergif')[0];
+// let songGif = document.getElementById('song-gif');
 
-let songItems = document.getElementsByClassName('songItem');
+// let songItems = document.getElementsByClassName('songItem');
 
-let songs = [
-    {songName:"let me down slowly",songDuration:"0.5:34",filepath:"/sounds/song/0.mp3", coverPath: "/images/covers/1.jpg"},
-    {songName:"Excuses",songDuration:"0.2:56",filepath:"/sounds/song/1.mp3", coverPath: "/images/covers/2.jpg"},
-    {songName:"Love Nwantiti",songDuration:"0.2:42",filepath:"/sounds/song/2.mp3", coverPath: "/images/covers/3.jpg"},
-    {songName:"let me down slowly",songDuration:"0.5:34",filepath:"/sounds/song/0.mp3", coverPath: "/images/covers/4.jpg"},
-    {songName:"Excuses",songDuration:"0.2:56",filepath:"/sounds/song/1.mp3", coverPath: "/images/covers/5.jpg"},
-    {songName:"Love Nwantiti ",songDuration:"0.2:42",filepath:"/sounds/song/2.mp3", coverPath: "/images/covers/6.jpg"},
-    {songName:"let me down slowly ",songDuration:"0.5:34",filepath:"/sounds/song/0.mp3", coverPath: "/images/covers/1.jpg"},
-    {songName:"Love Nwantiti",songDuration:"0.2:42",filepath:"/sounds/song/2.mp3", coverPath: "/images/covers/2.jpg"},
-    {songName:"let me down slowly",songDuration:"0.5:34",filepath:"/sounds/song/0.mp3", coverPath: "/images/covers/1.jpg"},
-    {songName:"Excuses",songDuration:"0.2:56",filepath:"/sounds/song/1.mp3", coverPath: "/images/covers/2.jpg"},
-]
+// let songs = [
+//     {songName:"let me down slowly",songDuration:"0.5:34",filepath:"/sounds/song/0.mp3", coverPath: "/images/covers/1.jpg"},
+//     {songName:"Excuses",songDuration:"0.2:56",filepath:"/sounds/song/1.mp3", coverPath: "/images/covers/2.jpg"},
+//     {songName:"Love Nwantiti",songDuration:"0.2:42",filepath:"/sounds/song/2.mp3", coverPath: "/images/covers/3.jpg"},
+//     {songName:"let me down slowly",songDuration:"0.5:34",filepath:"/sounds/song/0.mp3", coverPath: "/images/covers/4.jpg"},
+//     {songName:"Excuses",songDuration:"0.2:56",filepath:"/sounds/song/1.mp3", coverPath: "/images/covers/5.jpg"},
+//     {songName:"Love Nwantiti ",songDuration:"0.2:42",filepath:"/sounds/song/2.mp3", coverPath: "/images/covers/6.jpg"},
+//     {songName:"let me down slowly ",songDuration:"0.5:34",filepath:"/sounds/song/0.mp3", coverPath: "/images/covers/1.jpg"},
+//     {songName:"Love Nwantiti",songDuration:"0.2:42",filepath:"/sounds/song/2.mp3", coverPath: "/images/covers/2.jpg"},
+//     {songName:"let me down slowly",songDuration:"0.5:34",filepath:"/sounds/song/0.mp3", coverPath: "/images/covers/1.jpg"},
+//     {songName:"Excuses",songDuration:"0.2:56",filepath:"/sounds/song/1.mp3", coverPath: "/images/covers/2.jpg"},
+// ]
  
-console.log(songItems.length);
+// console.log(songItems.length);
 
-//----------showing the songs on screen--------
-for(let i=0;i<songItems.length;i++){
-    songItems[i].getElementsByTagName('img')[0].src = songs[i].coverPath;
-    songItems[i].getElementsByClassName('SongName')[0].innerText = songs[i].songName;
-    songItems[i].getElementsByClassName('song-duration')[0].innerText = songs[i].songDuration;
-}
+// //----------showing the songs on screen--------
+// for(let i=0;i<songItems.length;i++){
+//     songItems[i].getElementsByTagName('img')[0].src = songs[i].coverPath;
+//     songItems[i].getElementsByClassName('SongName')[0].innerText = songs[i].songName;
+//     songItems[i].getElementsByClassName('song-duration')[0].innerText = songs[i].songDuration;
+// }
 
 
 
-const setDefault = ()=>{
-    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
-        element.classList.remove('fa-pause-circle');
-        element.classList.add('fa-circle-play');
-     });
-    }
+// const setDefault = ()=>{
+//     Array.from(document.getElementsByClassName('songItemPlay')).forEach((element)=>{
+//         element.classList.remove('fa-pause-circle');
+//         element.classList.add('fa-circle-play');
+//      });
+//     }
     
-let songInfoName = document.getElementById('song-info-name');
-let songInfoDuration = document.getElementById('song-info-duration');
+// let songInfoName = document.getElementById('song-info-name');
+// let songInfoDuration = document.getElementById('song-info-duration');
 
-const setAudioPath = function(){
+// const setAudioPath = function(){
 
-    songInfoName.innerText = songs[songIndex].songName;
-    songInfoDuration.innerText = songs[songIndex].songDuration;
-    songBannergif.getElementsByTagName('img')[0].src = songs[songIndex].coverPath;
-    songBannergif.classList.add('border');
-    songGif.classList.remove('opacity');
-     audioElement.src = '/sounds/song/' + (songIndex)+'.mp3';
-    //audioElement.src = songs[songIndex].filepath;
-    //  audioElement =new Audio('/sounds/song/' + (songIndex) + '.mp3');
-
-
-}
-
-const idealCondition = function(){
-    currentPlay.classList.remove('fa-circle-play');
-    currentPlay.classList.add('fa-pause-circle');
-     //changeSongIcon();
-     songIcon[songIndex].classList.remove('fa-circle-play');
-     songIcon[songIndex].classList.add('fa-pause-circle');
-
-}
+//     songInfoName.innerText = songs[songIndex].songName;
+//     songInfoDuration.innerText = songs[songIndex].songDuration;
+//     songBannergif.getElementsByTagName('img')[0].src = songs[songIndex].coverPath;
+//     songBannergif.classList.add('border');
+//     songGif.classList.remove('opacity');
+//      audioElement.src = '/sounds/song/' + (songIndex)+'.mp3';
+//     //audioElement.src = songs[songIndex].filepath;
+//     //  audioElement =new Audio('/sounds/song/' + (songIndex) + '.mp3');
 
 
+// }
 
-//-----------------change the icons play to pause if song is playing-----
-let songIcon = document.getElementsByClassName('songItemPlay');
+// const idealCondition = function(){
+//     currentPlay.classList.remove('fa-circle-play');
+//     currentPlay.classList.add('fa-pause-circle');
+//      //changeSongIcon();
+//      songIcon[songIndex].classList.remove('fa-circle-play');
+//      songIcon[songIndex].classList.add('fa-pause-circle');
 
-//---------------bottom----------------------
-const currentPlayIconChange = function (){
-    if(audioElement.paused || audioElement.currentTime <=0){
+// }
+
+
+
+// //-----------------change the icons play to pause if song is playing-----
+// let songIcon = document.getElementsByClassName('songItemPlay');
+
+// //---------------bottom----------------------
+// const currentPlayIconChange = function (){
+//     if(audioElement.paused || audioElement.currentTime <=0){
        
-        idealCondition();
-        songGif.classList.remove('opacity');
-         audioElement.play();
-    }
-    else{
-        currentPlay.classList.remove('fa-pause-circle');
-        currentPlay.classList.add('fa-circle-play');
-        //changeSongIcon();
-        songGif.classList.add('opacity');
-        songIcon[songIndex].classList.remove('fa-pause-circle');
-        songIcon[songIndex].classList.add('fa-circle-play');
-        audioElement.pause();
-    }
-};
+//         idealCondition();
+//         songGif.classList.remove('opacity');
+//          audioElement.play();
+//     }
+//     else{
+//         currentPlay.classList.remove('fa-pause-circle');
+//         currentPlay.classList.add('fa-circle-play');
+//         //changeSongIcon();
+//         songGif.classList.add('opacity');
+//         songIcon[songIndex].classList.remove('fa-pause-circle');
+//         songIcon[songIndex].classList.add('fa-circle-play');
+//         audioElement.pause();
+//     }
+// };
 
-currentPlay.addEventListener('click',function(){
-    currentPlayIconChange();
-});
+// currentPlay.addEventListener('click',function(){
+//     currentPlayIconChange();
+// });
 
-prevPlay.addEventListener('click',function(){
-        songIndex--;
-        setDefault();
-        idealCondition();
-        setAudioPath();
-        audioElement.play();      
+// prevPlay.addEventListener('click',function(){
+//         songIndex--;
+//         setDefault();
+//         idealCondition();
+//         setAudioPath();
+//         audioElement.play();      
     
-});
+// });
 
-nextPlay.addEventListener('click',function(){
-    songIndex++;
-    setDefault();
-    idealCondition();
-    setAudioPath();
-    audioElement.play();
-});
+// nextPlay.addEventListener('click',function(){
+//     songIndex++;
+//     setDefault();
+//     idealCondition();
+//     setAudioPath();
+//     audioElement.play();
+// });
 
 
 
- const bottom = document.getElementsByClassName('bottom')[0];
-//---------
-    Array.from(document.getElementsByClassName('songItem')).forEach((element)=>{
-        element.addEventListener('click',(e)=>{
+//  const bottom = document.getElementsByClassName('bottom')[0];
+// //---------
+//     Array.from(document.getElementsByClassName('songItem')).forEach((element)=>{
+//         element.addEventListener('click',(e)=>{
          
-          //let songItemplayIconElement = element.children[2];
+//           //let songItemplayIconElement = element.children[2];
           
-          songIndex = element.getAttribute("value");  
-          console.log(songIndex);
-          bottom.classList.remove('display');
+//           songIndex = element.getAttribute("value");  
+//           console.log(songIndex);
+//           bottom.classList.remove('display');
 
-          setDefault();
-          setAudioPath(); 
-          currentPlayIconChange();
-        })
-    });
+//           setDefault();
+//           setAudioPath(); 
+//           currentPlayIconChange();
+//         })
+//     });
 
 
-    //--------------- updating range---------------
-audioElement.addEventListener('timeupdate',()=>{
-    progress = parseInt((audioElement.currentTime / audioElement.duration)*100); 
-    progressBar.value = progress;
-   // console.log(progress);
-});
+//     //--------------- updating range---------------
+// audioElement.addEventListener('timeupdate',()=>{
+//     progress = parseInt((audioElement.currentTime / audioElement.duration)*100); 
+//     progressBar.value = progress;
+//    // console.log(progress);
+// });
 
-//----------------if someone change the range element---------
-progressBar.addEventListener('change',()=>{
-    audioElement.currentTime = ((progressBar.value * audioElement.duration) /100);
-});
+// //----------------if someone change the range element---------
+// progressBar.addEventListener('change',()=>{
+//     audioElement.currentTime = ((progressBar.value * audioElement.duration) /100);
+// });
 
     //console.log(songItemplayIconElement);
           //console.log(songItemplayIconElement.getElementsByClassName('songItemPlay')[0].innerText);
