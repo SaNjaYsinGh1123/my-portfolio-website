da38a04668fb45165dcea2c81413eb09d5cad3f3
SaNjaYsinGh1123 <sanjayfbd93544@gmail.com>
2023-01-11 16:09:41 +0530

    try 17 (with app.use('/sounds'), sounds music.js, socket_engine,chat_socket path in mongostore and view ,4 update with home-portfoilio.ejs) vercel.json and delete echo build

--- assets/javascript/music.js
+++ assets/javascript/music.js
@@ -53,8 +53,8 @@ const setAudioPath = function(){
     songBannergif.getElementsByTagName('img')[0].src = songs[songIndex].coverPath;
     songBannergif.classList.add('border');
     songGif.classList.remove('opacity');
-    // audioElement.src = '/sounds/song/' + (songIndex)+'.mp3';
-    audioElement.src = songs[songIndex].filepath;
+     audioElement.src = '/sounds/song/' + (songIndex)+'.mp3';
+    //audioElement.src = songs[songIndex].filepath;
     //  audioElement =new Audio('/sounds/song/' + (songIndex) + '.mp3');
 
 
