819b802b3094c04eab7279cf1fb7e44a7341fd85
SaNjaYsinGh1123 <sanjayfbd93544@gmail.com>
2023-01-13 09:39:11 +0530

    try 36

--- assets/javascript/chat_engine.js
+++ assets/javascript/chat_engine.js
@@ -21,7 +21,7 @@ class ChatEngine{
         this.chatBoxId = chatBoxId;
         this.userName = userName;
 
-        this.socket = io.connect('https://my-portfolio-website-BEASTMODEON.vercel.app' + process.env.PORT);
+        this.socket = io.connect('https://my-portfolio-website-BEASTMODEON.vercel.app');
         if(this.userName){
         this.connectionHandler();
         }
